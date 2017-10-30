0f3edb1fb2a15f42110398361ba9813154b73d57
Marcus Longmuir <marcuslongmuir@me.com>
2017-10-30 22:37:05 +0000

    Updated example

--- example/ts/src/index.ts
+++ example/ts/src/index.ts
@@ -1,4 +1,4 @@
-import {grpc, Code, Metadata} from "../../../ts/src/index";
+import {grpc} from "grpc-web-client";
 import {BookService} from "../_proto/examplecom/library/book_service_pb_service";
 import {QueryBooksRequest, Book, GetBookRequest} from "../_proto/examplecom/library/book_service_pb";
 
@@ -15,7 +15,7 @@ function getBook() {
       const { status, statusMessage, headers, message, trailers } = res;
       console.log("getBook.onEnd.status", status, statusMessage);
       console.log("getBook.onEnd.headers", headers);
-      if (status === Code.OK && message) {
+      if (status === grpc.Code.OK && message) {
         console.log("getBook.onEnd.message", message.toObject());
       }
       console.log("getBook.onEnd.trailers", trailers);
@@ -32,14 +32,15 @@ function queryBooks() {
   const client = grpc.client(BookService.QueryBooks, {
     host: host,
   });
-  client.onHeaders((headers: Metadata) => {
+  client.onHeaders((headers: grpc.Metadata) => {
     console.log("queryBooks.onHeaders", headers);
   });
   client.onMessage((message: Book) => {
     console.log("queryBooks.onMessage", message.toObject());
   });
-  client.onEnd((code: Code, msg: string, trailers: Metadata) => {
+  client.onEnd((code: grpc.Code, msg: string, trailers: grpc.Metadata) => {
     console.log("queryBooks.onEnd", code, msg, trailers);
   });
   client.start();
+  client.send(queryBooksRequest);
 }
